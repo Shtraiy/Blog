230b75dc294812d8e4642cdca26e5d7e0d55723e
Shtraiy <50954859+Shtraiy@users.noreply.github.com>
2024-01-14 07:59:23 +0800

    1.14 updated

--- public/js/search.js
+++ public/js/search.js
@@ -109,7 +109,7 @@ var searchFunc = function(path, searchId, contentId) {
           if (matches > 0) {
             var searchResult = {};
             searchResult.rank = matches;
-            searchResult.str = "<li><a href='"+ "/" + dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
+            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
             if (firstOccur >= 0) {
               // cut out 100 characters
               var start = firstOccur - 20;
@@ -127,7 +127,7 @@ var searchFunc = function(path, searchId, contentId) {
                 end = dataContent.length;
               }
 
-              var matchContent = dataContent.substr(start, end);
+              var matchContent = dataContent.substring(start, end);
 
               // highlight all keywords
               var regS = new RegExp(keywords.join("|"), "gi");
